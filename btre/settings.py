af77b5ce869f2b1c2521acf93b93fb6b338b0163
Kiran Karandikar <kiran@noreply.com>
2021-06-12 12:36:37 +0530

    Changed database to postgres

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "btree_db",
+        "USER": "postgres",
+        "PASSWORD": "Kiran8796",
+        "HOST": "localhost",
     }
 }
 
