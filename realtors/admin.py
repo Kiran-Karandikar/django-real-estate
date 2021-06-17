8d969974e9d7e33fd9befb2c12c37f2d1e73f2cc
Kiran Karandikar <38068652+Kiran-Karandikar@users.noreply.github.com>
2021-06-17 19:58:04 +0530

    Added display details for models in admin page

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Realtor
 
+
+class RealtorAdmin(admin.ModelAdmin):
+    """
+    Docstring.
+    """
+    list_display = ("id", "name", "is_mvp", "phone", "email")
+    list_display_links = ("id", "name")
+    list_editable = ("is_mvp",)
+    list_filter = ("is_mvp", )
+    list_per_page = 10
+    # todo
+    # this shows the fields to search for just one field not for more than field
+    search_fields = ("name",)
+
+
 # Register your models here.
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
+
