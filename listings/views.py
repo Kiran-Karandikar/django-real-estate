79627e4b9d978d4f3680eb6d1dd17b0087ab74b0
Kiran Karandikar <38068652+Kiran-Karandikar@users.noreply.github.com>
2021-06-20 17:29:38 +0530

    Implemented pagination

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,7 @@
 """
 # Docstring
 """
-# todo -> pagination imports
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Listing
 # Create your views here.
@@ -17,16 +16,16 @@ def base_listings(request):
     Returns:
 
     """
-    all_listings = Listing.objects.all()
-    # todo
-    # Implementing pagination
-
-    # paginated = Paginator(all_listings, 2)
-    # request_page = request.GET.get("page")
-    # paginated_listings = paginated.get_page(request_page)
+    all_listings = Listing.objects.order_by('-list_date').filter(
+        is_published=True
+    )
+    paginated = Paginator(all_listings, 2)
+    requested_page = request.GET.get("page")
+    paginated_listings = paginated.get_page(requested_page)
 
     context = {
-            "listings": all_listings,
+            # "listings": all_listings,
+            "listings": paginated_listings,
             # "listings_temp": paginated_listings,
     }
     return render(request, "listings/listings.html", context)
