a018cec3fd11e7dde11048742ae94873f6746035
Kiran Karandikar <38068652+Kiran-Karandikar@users.noreply.github.com>
2021-06-22 09:59:48 +0530

    Fixed the bugs in listing search

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,6 @@
 """
 # Docstring
 """
-from uuid import uuid1
-
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
@@ -61,38 +59,63 @@ def search(request, **kwargs):
     """
     search_result_set = Listing.objects.order_by("-list_date").filter(
         is_published = True)
-
+    context = {
+            "us_states": us_states, "bedrooms": bedrooms,
+            "max_price": max_price,
+    }
     if 'keywords' in request.GET:
         keywords = request.GET["keywords"]
-        if keywords:
+        if keywords and search_result_set:
             search_result_set = search_result_set.filter(
                 description__icontains = keywords)
     if 'city' in request.GET:
         city = request.GET["city"]
-        if city:
+        if city and search_result_set:
             search_result_set = search_result_set.filter(
                 city__iexact = city.strip())
     if 'state' in request.GET:
         state = request.GET["state"]
-        if state:
+        if state and search_result_set:
             search_result_set = search_result_set.filter(
                 state__iexact = state.strip())
     if 'bedrooms' in request.GET:
         bedroom = int(request.GET["bedrooms"])
-        if bedroom:
+        if bedroom and search_result_set:
             search_result_set = search_result_set.filter(
                 bedrooms__lte = bedroom)
     if 'price' in request.GET:
         price = int(request.GET["price"])
-        if price:
+        if price and search_result_set:
             search_result_set = search_result_set.filter(price__lte = price)
+
+    context["search_result"] = search_result_set
+    context["GetRequestValue"] = request.GET
+
+    # feature
+    # how to implement cookies to store the values of search fields across
+    # multiple requests -->
+    # This is for first page, since on first page the get request will have
+    # search fields but not `page` and on subsequents paginated pages the get
+    # request will not have any of search fields resulting in clearing of
+    # the search form.
+
+    # if "page" not in request.GET:
+    #     context["GetRequestValue"] = request.GET
+
+    # feature
+    # this is test pagination
+
+    # paginated_search_result = Paginator(search_result_set, 2)
+    # requested_page = 1
+    # if "page" in request.GET:
+    #     requested_page = request.GET["page"]
+    # search_result_set = paginated_search_result.get_page(requested_page)
+
+    # bug
+    # since in template `search_result_set` is iterated, need to find a way
+    # to make it iterable in case it is single item
 
     # if len(search_result_set) == 1:
     #     search_result_set = list(search_result_set)
 
-    context = {
-            "us_states"    : us_states, "bedrooms": bedrooms,
-            "max_price"    : max_price, "random": uuid1(),
-            "search_result": search_result_set, "GetRequestValue": request.GET
-    }
     return render(request, "listings/search.html", context)
